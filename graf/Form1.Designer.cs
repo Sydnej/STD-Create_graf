8c5a83b62bfa9464a6dce393bd87cb774de3aea2
Sydnej <sydnej.w@gmail.com>
2017-04-26 18:04:59 +0200

    git

---
+++ graf/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace graf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_N = new System.Windows.Forms.TextBox();
+            this.textBox_R = new System.Windows.Forms.TextBox();
+            this.textBox_rr = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.N = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_N
+            // 
+            this.textBox_N.Location = new System.Drawing.Point(13, 43);
+            this.textBox_N.Name = "textBox_N";
+            this.textBox_N.Size = new System.Drawing.Size(56, 20);
+            this.textBox_N.TabIndex = 0;
+            // 
+            // textBox_R
+            // 
+            this.textBox_R.Location = new System.Drawing.Point(75, 43);
+            this.textBox_R.Name = "textBox_R";
+            this.textBox_R.Size = new System.Drawing.Size(56, 20);
+            this.textBox_R.TabIndex = 1;
+            // 
+            // textBox_rr
+            // 
+            this.textBox_rr.Location = new System.Drawing.Point(137, 43);
+            this.textBox_rr.Name = "textBox_rr";
+            this.textBox_rr.Size = new System.Drawing.Size(56, 20);
+            this.textBox_rr.TabIndex = 2;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(200, 43);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 3;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // N
+            // 
+            this.N.AutoSize = true;
+            this.N.Location = new System.Drawing.Point(13, 24);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(15, 13);
+            this.N.TabIndex = 4;
+            this.N.Text = "N";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "r";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 540);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(281, 43);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(75, 23);
+            this.button_connect.TabIndex = 8;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(363, 43);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(75, 23);
+            this.button_save.TabIndex = 9;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 662);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.N);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.textBox_rr);
+            this.Controls.Add(this.textBox_R);
+            this.Controls.Add(this.textBox_N);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_N;
+        private System.Windows.Forms.TextBox textBox_R;
+        private System.Windows.Forms.TextBox textBox_rr;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Label N;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ graf/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Xml;
+
+namespace graf
+{
+    public partial class Form1 : Form
+    {
+        public class MyPoint
+        {
+            public int PointId { get; set; }
+            public int PointX { get; set; }
+            public int PointY { get; set; }
+
+            public MyPoint(int id, int x, int y)
+            {
+                PointId = id;
+                PointX = x;
+                PointY = y;
+            }
+        }
+
+        Bitmap DrawArea;
+        private List<MyPoint> points;
+        private Dictionary<int, List<int>> EdgeToXml;
+        private int?[,] Edge;
+        private List<int> NodeToXml;
+        public Form1() 
+        {
+            InitializeComponent();
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            points = RandomPoints(Int32.Parse(textBox_N.Text), Int32.Parse(textBox_R.Text));
+
+            Graphics g;
+            g = Graphics.FromImage(DrawArea);
+
+            Pen mypen = new Pen(Color.DarkRed);
+
+            foreach (MyPoint point in points)
+            {
+                g.DrawEllipse(mypen, point.PointX + 250, point.PointY + 250, 1, 1);
+            }
+
+            pictureBox1.Image = DrawArea;
+
+            g.Dispose();
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            NodeToXml = new List<int>();
+            EdgeToXml = new Dictionary<int, List<int>>();
+            Edge = new int?[Int32.Parse(textBox_N.Text), Int32.Parse(textBox_N.Text)];
+
+            Graphics g;
+            g = Graphics.FromImage(DrawArea);
+            Pen line = new Pen(Color.Black);
+            Pen mypen = new Pen(Color.DarkRed);
+            float r = float.Parse(textBox_rr.Text);
+            foreach (MyPoint point in points)
+            {
+                NodeToXml.Add(point.PointId);
+                foreach (MyPoint point2 in points)
+                {
+                    double distance = Distance(point, point2);
+                    if (distance != 0 && distance < r)
+                    {
+                        if (!Edge[point.PointId, point2.PointId].HasValue && !Edge[point2.PointId, point.PointId].HasValue)
+                        {
+                            Edge[point.PointId, point2.PointId] = 1;
+                            if (!EdgeToXml.ContainsKey(point.PointId))
+                            {
+                                EdgeToXml.Add(point.PointId, new List<int>());
+                                EdgeToXml[point.PointId].Add(point2.PointId);
+                            }
+                            else
+                            {
+                                EdgeToXml[point.PointId].Add(point2.PointId);
+                            }
+                            g.DrawLine(line, point.PointX + 250, point.PointY + 250, point2.PointX + 250, point2.PointY + 250);
+                        }
+                    }
+                }
+            }
+            foreach (MyPoint point in points)
+            {
+                g.DrawEllipse(mypen, point.PointX + 250 - r/2, point.PointY + 250 - r / 2, r, r);
+            }
+            pictureBox1.Image = DrawArea;
+            g.Dispose();
+        }
+
+        private void SaveToGXL(string file)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            XmlWriter xmlWriter = XmlWriter.Create(file, settings);
+
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("gxl");
+            xmlWriter.WriteStartElement("graph");
+            xmlWriter.WriteAttributeString("id", "graph");
+
+            foreach (int i in NodeToXml)
+            {
+                xmlWriter.WriteStartElement("node");
+                xmlWriter.WriteAttributeString("id", i.ToString());
+                xmlWriter.WriteEndElement();
+            }
+            foreach (var edge in EdgeToXml.Keys)
+            {
+                Console.WriteLine("krawedz: " + edge);
+                Console.WriteLine(EdgeToXml[edge].Count);
+                foreach (int i in EdgeToXml[edge])
+                {
+                    xmlWriter.WriteStartElement("edge");
+                    xmlWriter.WriteAttributeString("from", edge.ToString());
+                    xmlWriter.WriteAttributeString("to", i.ToString());
+                    xmlWriter.WriteEndElement();
+                }
+            }
+            xmlWriter.WriteEndElement();
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "GXL-File | *.gxl";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SaveToGXL(saveFileDialog.FileName);
+                MessageBox.Show("Saved", "Save", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        public static List<MyPoint> RandomPoints(int N, int R)
+        {
+            List<MyPoint> points = new List<MyPoint>();
+            Random gen = new Random();
+            while (points.Count < N)
+            {
+                int x = gen.Next(-R, R);
+                int y = gen.Next(-R, R);
+
+                if ((x * x + y * y) <= R * R)
+                {
+                    points.Add(new MyPoint(points.Count, x, y));
+                }
+            }
+
+            return points;
+        }
+
+        public static double Distance(MyPoint pointA, MyPoint pointB)
+        {
+            double d1 = pointA.PointX - pointB.PointX;
+            double d2 = pointA.PointY - pointB.PointY;
+
+            return Math.Sqrt(d1 * d1 + d2 * d2);
+        }
+    }
+
+}
